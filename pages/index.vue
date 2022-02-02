a16dd472e455e8b61b593607c7415ca139644ff7
Massimo Pegoraro <massimo.pegoraro@atoms.studio>
2022-02-02 10:26:26 +0100

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <p>Data: <pre>{{ data }}</pre></p>
+  </div>
+  <nuxt-link to="/user">User</nuxt-link>
+</template>
+
+<script setup>
+const { data } = await useAsyncData('data', () => $fetch('https://reqres.in/api/unknown/3?delay=2'), { lazy: true})
+</script>
